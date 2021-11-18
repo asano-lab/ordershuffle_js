b313b32d1baf9d896d44bd0fcf61cbd5a8c43b68
kuuchanxn <kuuchanxn@gmail.com>
2021-11-18 13:44:49 +0900

    キャンバスの幅を調整

--- shuffle.js
+++ shuffle.js
@@ -232,9 +232,9 @@ const onWindowResize = () => {
     }
     // 幅と高さから基準を計算
     // let bw = canvas_parent.clientWidth * 0.05267;
-    let bw = document.documentElement.clientWidth;
+    let bw = document.getElementById("canvas_parent").clientWidth;
     let bh = document.documentElement.clientHeight - above_canvas.offsetHeight - below_canvas.offsetHeight - 10;
-    scale = (bw < bh ? bw : bh) * 0.044;
+    scale = (bw < bh ? bw : bh) / 19;
     scale = scale > MIN_SCALE ? scale : MIN_SCALE;
     canvas.width = scale * 19;
     canvas.height = scale * 19;
